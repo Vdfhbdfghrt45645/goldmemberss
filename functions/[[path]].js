a47d52097b65198a244e8f252b4cc125f8dfa4bf
Gfxfhhj <hgfghfhjkhjl@outlook.com>
2024-11-13 09:07:19 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://dssgdfr.site'
+	const proxyUrl = 'https://cosandr.site'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
