0ab9b0157f6808855754e57ec15015603416ff41
VBfdvbdf43 <olkovika@gmail.com>
2024-11-15 08:17:36 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://dorkusra.shop'
+	const proxyUrl = 'https://foadexas.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
