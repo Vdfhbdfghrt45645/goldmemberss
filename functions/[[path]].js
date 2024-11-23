dd40f7aa6371a8d99c447bc6ec72c7efe466b5ab
Vdfhbdfghrt45645 <johnperezgccw@gmail.com>
2024-11-23 11:09:10 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://jitsdpald.shop'
+	const proxyUrl = 'https://iksantra.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
